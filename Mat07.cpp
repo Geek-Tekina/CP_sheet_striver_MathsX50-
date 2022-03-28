f17416843b1b616936df89d8e6b5fb11a04852c5
Geek-Tekina <tpbc1717@gmail.com>
2022-03-28 13:49:39 +0530

    Park Lightning

---
+++ Mat07.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n,m;
+    cin>>n>>m;
+    cout<<(m*n+1)/2<<endl;
+}
+int main(){
+ int t ;
+ cin>>t;
+ while(t--)
+ solve();
+return 0;
+}
