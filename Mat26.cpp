a5b973c554cf029b2fa7b02a187cfbfee066e249
Geek-Tekina <tpbc1717@gmail.com>
2022-04-08 14:07:35 +0530

    Digital Root

---
+++ Mat26.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    long long t,x;
+    cin>>t>>x;
+    cout<<(t-1)*9+x<<endl;
+}
+int main(){
+ int t;
+ cin>>t;
+ while(t--)
+ solve();
+return 0;
+}
