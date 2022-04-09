02b2c49428fc1bd591fd8c4d41a4276a808a400c
Geek-Tekina <tpbc1717@gmail.com>
2022-04-09 16:01:11 +0530

    Heating

---
+++ Mat27.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int c, sum;
+
+inline bool read() {
+	if(!(cin >> c >> sum))
+		return false;
+	return true;
+}
+
+inline void solve() {
+	int d = sum / c;
+	int rem = sum % c;
+	cout << rem * (d + 1) * (d + 1) + (c - rem) * d * d << endl;
+}
+
+int main() {
+
+	int n; cin >> n;
+	while(n--) {
+		read();
+		solve();
+	}
+	return 0;
+}
